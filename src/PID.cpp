3bbaab7fb59a8ce5db516bf20e2c2c0c9074dd2a
shreyasrivastava17 <35628910+shreyasrivastava17@users.noreply.github.com>
2018-04-13 01:14:52 +0530

    Add files via upload

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,23 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init() {
+    Kp = 0.16;
+    Ki = 0.0002;
+    Kd = 3.1;
+    p_error = 0.0;
+    d_error = 0.0;
+    i_error = 0.0;
+    
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    return -(Kp*p_error + Kd*d_error + Ki*i_error);
 }
 
--- src/PID.h
+++ src/PID.h
@@ -30,7 +30,7 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init();
 
   /*
   * Update the PID error variables given cross track error.
